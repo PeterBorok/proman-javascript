fa83397eef3e409fce955644c72ad0ac8a91ed09
Sophie <szaniszlozs7@gmail.com>
2020-01-06 16:48:52 +0100

    changed sql

--- data/promanData.sql
+++ data/promanData.sql
@@ -5,63 +5,46 @@
 -- Dumped from database version 9.5.6
 -- Dumped by pg_dump version 9.5.6
 
-ALTER TABLE IF EXISTS ONLY public.boards
-    DROP CONSTRAINT IF EXISTS pk_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.cards
-    DROP CONSTRAINT IF EXISTS pk_card_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.statuses
-    DROP CONSTRAINT IF EXISTS fk_card_id CASCADE;
+DROP TABLE IF EXISTS boards;
+DROP TABLE IF EXISTS cards;
+DROP TABLE IF EXISTS statuses;
 
-DROP TABLE IF EXISTS public.boards;
-DROP SEQUENCE IF EXISTS public.board_id_seq;
+
 CREATE TABLE boards
 (
-    id    serial NOT NULL,
+    id    SERIAL PRIMARY KEY NOT NULL,
     title text
 );
 
-DROP TABLE IF EXISTS public.cards;
-DROP SEQUENCE IF EXISTS public.card_id_seq;
+
 CREATE TABLE cards
 (
-    id        serial NOT NULL,
-    board_id  int    NOT NULL,
+    id        SERIAL PRIMARY KEY NOT NULL,
+    board_id  int                NOT NULL,
     title     text,
-    status_id int    NOT NULL,
-    order     int
+    status_id int                NOT NULL,
+    orderd    int
 );
 
-DROP TABLE IF EXISTS public.statuses;
-DROP SEQUENCE IF EXISTS public.status_id_seq;
+
 CREATE TABLE statuses
 (
-    id    serial NOT NULL,
+    id    SERIAL PRIMARY KEY NOT NULL,
     title text
 );
 
 
 ALTER TABLE ONLY boards
-    ADD CONSTRAINT pk_boards_id PRIMARY KEY (id);
-
-ALTER TABLE ONLY cards
-    ADD CONSTRAINT pk_cards_id PRIMARY KEY (id);
+    ADD CONSTRAINT fk_card_id FOREIGN KEY (id) REFERENCES cards (board_id);
 
 ALTER TABLE ONLY statuses
-    ADD CONSTRAINT pk_statuses_id PRIMARY KEY (id);
-
-
-ALTER TABLE ONLY cards
-    ADD CONSTRAINT fk_boards_id FOREIGN KEY (board_id) REFERENCES boards (id);
-
-ALTER TABLE ONLY cards
-    ADD CONSTRAINT fk_statuses_id FOREIGN KEY (status_id) REFERENCES statuses (id);
+    ADD CONSTRAINT fk_status_id FOREIGN KEY (id) REFERENCES cards (status_id);
 
 
 INSERT INTO boards
 VALUES (1, 'Board 1');
 INSERT INTO boards
 VALUES (2, 'Board 2');
-SELECT pg_catalog.setval('boards', 3, true);
 
 INSERT INTO cards
 VALUES (1, 1, 'new card 1', 0, 0);
@@ -87,9 +70,6 @@ INSERT INTO cards
 VALUES (11, 2, 'done card 1', 3, 0);
 INSERT INTO cards
 VALUES (12, 2, 'done card 1', 3, 1);
-SELECT pg_catalog.setval('cards', 2, true);
-
-
 
 INSERT INTO statuses
 VALUES (0, 'new');
@@ -99,5 +79,4 @@ INSERT INTO statuses
 VALUES (2, 'testing');
 INSERT INTO statuses
 VALUES (3, 'done');
-SELECT pg_catalog.setval('statuses', 3, true);
 
