013a2f62c65aad47eb8a6e90bda7b0eb74cce213
Sophie <szaniszlozs7@gmail.com>
2020-01-07 11:02:33 +0100

    deleted rows from cards, now, sql i think is working

--- data/promanData.sql
+++ data/promanData.sql
@@ -51,30 +51,10 @@ SELECT pg_catalog.setval('boards_id_seq', 2, true);
 INSERT INTO cards
 VALUES (1, 1, 'new card 1', 0, 0);
 INSERT INTO cards
-VALUES (2, 1, 'new card 2', 0, 1);
+VALUES (2, 2, 'new card 2', 0, 1);
 INSERT INTO cards
 VALUES (3, 1, 'in progress card', 1, 0);
-INSERT INTO cards
-VALUES (4, 1, 'planning', 2, 0);
-INSERT INTO cards
-VALUES (5, 1, 'done card 1', 3, 0);
-INSERT INTO cards
-VALUES (6, 1, 'done card 1', 3, 1);
-INSERT INTO cards
-VALUES (7, 2, 'new card 1', 0, 0);
-INSERT INTO cards
-VALUES (8, 2, 'new card 2', 0, 1);
-INSERT INTO cards
-VALUES (9, 2, 'in progress card', 1, 0);
-INSERT INTO cards
-VALUES (10, 2, 'planning', 2, 0);
-INSERT INTO cards
-VALUES (11, 2, 'done card 1', 3, 0);
-INSERT INTO cards
-VALUES (12, 2, 'done card 1', 3, 1);
-INSERT INTO cards
-VALUES (13,1, 'test', 0, 0);
-SELECT pg_catalog.setval('cards_id_seq', 13, true);
+SELECT pg_catalog.setval('cards_id_seq', 3, true);
 
 INSERT INTO statuses
 VALUES (0, 'new');
