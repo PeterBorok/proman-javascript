37983cc3d0ebf7ba4b2939d121e445c6d1780b0c
Sophie <szaniszlozs7@gmail.com>
2020-01-07 14:28:37 +0100

    deleted boards

--- data_handler.py
+++ data_handler.py
@@ -2,30 +2,37 @@ import connection
 from psycopg2 import sql
 import bcrypt
 
-def get_card_status(status_id):
-    """
-    Find the first status matching the given id
-    :param status_id:
-    :return: str
-    """
-    statuses = persistence.get_statuses()
-    return next((status['title'] for status in statuses if status['id'] == str(status_id)), 'Unknown')
+@connection.connection_handler
+def get_boards(cursor):
+    cursor.execute("""
+                    SELECT * FROM boards
+    """)
+    boards = cursor.fetchall()
+    return boards
+
+    # """
+    # Find the first status matching the given id
+    # :param status_id:
+    # :return: str
+    # """
+    # statuses = persistence.get_statuses()
+    # return next((status['title'] for status in statuses if status['id'] == str(status_id)), 'Unknown')
 
 
-def get_boards():
-    """
-    Gather all boards
-    :return:
-    """
-    return persistence.get_boards(force=True)
-
-
-def get_cards_for_board(board_id):
-    persistence.clear_cache()
-    all_cards = persistence.get_cards()
-    matching_cards = []
-    for card in all_cards:
-        if card['board_id'] == str(board_id):
-            card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
-            matching_cards.append(card)
-    return matching_cards
+# def get_boards():
+#     """
+#     Gather all boards
+#     :return:
+#     """
+#     return persistence.get_boards(force=True)
+#
+#
+# def get_cards_for_board(board_id):
+#     persistence.clear_cache()
+#     all_cards = persistence.get_cards()
+#     matching_cards = []
+#     for card in all_cards:
+#         if card['board_id'] == str(board_id):
+#             card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
+#             matching_cards.append(card)
+#     return matching_cards
