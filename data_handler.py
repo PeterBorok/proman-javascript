334befeb625853743769fa8ed3097dc3282e554f
Gabor-Jalsovszky <gabor.jalsovszky@gmail.com>
2020-01-07 15:25:24 +0100

    fix sql queries in data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -16,7 +16,7 @@ def get_boards(cursor):
 def get_cards(cursor, board_id):
     cursor.execute("""
                     SELECT * FROM cards
-                    WHERE board_id=$(board_id)s;
+                    WHERE board_id=%(board_id)s;
     """,
                    {"board_id": board_id})
     cards = cursor.fetchall()
@@ -27,7 +27,7 @@ def get_cards(cursor, board_id):
 def get_one_card(cursor, card_id):
     cursor.execute("""
                 SELECT * FROM cards
-                WHERE id=$(card_id)s;
+                WHERE id=%(card_id)s;
     """,
                    {"card_id": card_id})
     card = cursor.fetchone()
@@ -39,7 +39,7 @@ def change_status(cursor, card_id, status_id):
     cursor.execute("""
                 UPDATE cards
                 SET status_id=$(status_id)s
-                WHERE id=$(card_id)s;
+                WHERE id=%(card_id)s;
     """,
                    {"card_id": card_id, "status_id": status_id})
 
--- main.py
+++ main.py
@@ -15,24 +15,24 @@ def index():
     return render_template('index.html', boards=boards)
 
 
-# @app.route("/get-boards")
-# @json_response
-# def get_boards():
-#     """
-#     All the boards
-#     """
-#     return data_handler.get_boards()
-#
-#
-# @app.route("/get-cards/<int:board_id>")
-# @json_response
-# def get_cards_for_board(board_id: int):
-#     """
-#     All cards that belongs to a board
-#     :param board_id: id of the parent board
-#     """
-#     return data_handler.get_cards_for_board(board_id)
-#
+@app.route("/get-boards")
+@json_response
+def get_boards():
+    """
+    All the boards
+    """
+    return data_handler.get_boards()
+
+
+@app.route("/get-cards/<int:board_id>")
+@json_response
+def get_cards_for_board(board_id: int):
+    """
+    All cards that belongs to a board
+    :param board_id: id of the parent board
+    """
+    return data_handler.get_cards(board_id)
+
 
 def main():
     app.run(debug=True)
