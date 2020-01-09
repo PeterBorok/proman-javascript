1a5db05d7b5f95c0cb6107582502826101c1e070
Sophie <szaniszlozs7@gmail.com>
2020-01-09 16:54:21 +0100

    fixed css

--- static/js/dom.js
+++ static/js/dom.js
@@ -22,14 +22,14 @@ export let dom = {
         for (let board of boards) {
 
             boardList += `
-            <section class="board">
+            <section class="board-${board.id} board">
                 <div class="board-header"><span class="board-title">${board.title}</span>
                     <button class="board-add">Add Card</button>
                     <button class="board-toggle" data-number="${board.id}"><i class="fas fa-chevron-down"></i></button>
                 </div>
             </section>`;
 
-            dom.loadStatuses();
+            dom.loadStatuses(board.id);
         }
         const outerHtml = `
             <ul class="board-container">
@@ -39,63 +39,62 @@ export let dom = {
         let boardsContainer = document.querySelector('#boards');
         boardsContainer.insertAdjacentHTML("beforeend", outerHtml);
     },
-    loadStatuses: function () {
+    loadStatuses: function (boardId) {
         dataHandler.getStatuses(function (statuses) {
-            dom.showStatuses(statuses);
+            dom.showStatuses(statuses, boardId);
         });
     },
 
-    showStatuses: function (statuses) {
+    showStatuses: function (statuses, boardId) {
         let htmlStatusesString = '';
+        let boardColumns = document.createElement('div');
+        boardColumns.setAttribute('class', 'board-columns');
+        const section = document.querySelector(`.board-${boardId}`);
+        section.appendChild(boardColumns);
 
         for (let status of statuses) {
-            htmlStatusesString = `<div class="board-columns" >` +
-                `<div class="board-column-${status.id}">${status.title}</div>` +
-                `</div>`;
+            htmlStatusesString = `<div class="board-column">` +
+                `<div class="board-column-${status.id} board-column-title">${status.title}</div>` +
+                `</div></div>`;
             console.log(htmlStatusesString);
             let element = document.createElement('div');
             element.insertAdjacentHTML('beforeend', htmlStatusesString);
-            const selectBoard = document.querySelector(".board");
-            selectBoard.appendChild(element);
-
-            // consdt column = document.querySelector(`.board-column-${status.id}`);
-            // column.appendChild(element);
-            // console.log(column);
+            boardColumns.insertAdjacentHTML('beforeend', htmlStatusesString);
         }
 
     }
-    // loadCards: function (boardId) {
-    //     // retrieves cards and makes showCards called
-    //     dataHandler.getCardsByBoardId(boardId, function (cards) {
-    //         dom.showCards(boardId, cards);
-    //     })
-    // },
-    // showCards: function (boardId, cards) {
-    //     // shows the cards of a board
-    //     // it adds necessary event listeners also
-    //     let numOfBoardColumns = document.querySelectorAll('.board-column');
-    //     let cardList = "";
-    //
-    //     for (let i = 1; i <= numOfBoardColumns.length; i++) {
-    //         for (let item of cards) {
-    //             if (item.status_id == `${i}`) {
-    //                 let card = document.createElement('div');
-    //                 card.setAttribute('class', 'card');
-    //                 let cardRemove = document.createElement('div');
-    //                 cardRemove.setAttribute('class', 'card-remove');
-    //                 let trash = document.createElement('i');
-    //                 trash.setAttribute('class', "fas fa-trash-alt");
-    //                 let cardTitle = document.createElement('div');
-    //                 cardTitle.setAttribute('class', 'card-title');
-    //                 cardTitle.innerHTML = `${item.title}`;
-    //
-    //                 card.appendChild(cardRemove);
-    //                 cardRemove.appendChild(trash);
-    //                 card.appendChild(cardTitle);
-    //                 cardList += card;
-    //                 document.querySelector(`.board-column-container-${item.status_id}${item.board_id}`).appendChild(cardList)
-    //             }
-    //         }
-    //     }
-    // }
+    loadCards: function (boardId) {
+        // retrieves cards and makes showCards called
+        dataHandler.getCardsByBoardId(boardId, function (cards) {
+            dom.showCards(boardId, cards);
+        })
+    },
+    showCards: function (boardId, cards) {
+        // shows the cards of a board
+        // it adds necessary event listeners also
+        let numOfBoardColumns = document.querySelectorAll('.board-column');
+        let cardList = "";
+
+        for (let i = 1; i <= numOfBoardColumns.length; i++) {
+            for (let item of cards) {
+                if (item.status_id == `${i}`) {
+                    let card = document.createElement('div');
+                    card.setAttribute('class', 'card');
+                    let cardRemove = document.createElement('div');
+                    cardRemove.setAttribute('class', 'card-remove');
+                    let trash = document.createElement('i');
+                    trash.setAttribute('class', "fas fa-trash-alt");
+                    let cardTitle = document.createElement('div');
+                    cardTitle.setAttribute('class', 'card-title');
+                    cardTitle.innerHTML = `${item.title}`;
+
+                    card.appendChild(cardRemove);
+                    cardRemove.appendChild(trash);
+                    card.appendChild(cardTitle);
+                    cardList += card;
+                    document.querySelector(`.board-column-container-${item.status_id}${item.board_id}`).appendChild(cardList)
+                }
+            }
+        }
+    }
 };
