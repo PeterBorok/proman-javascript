90022ff8381b3bcdc7fa424ff3157ae38f105abd
PeterBorok <peter.borok@gmail.com>
2020-01-21 14:13:20 +0100

    add delete button

--- static/js/dom.js
+++ static/js/dom.js
@@ -30,6 +30,7 @@ export let dom = {
             <section class="board-${board.id} board">
                 <div class="board-header"><span class="board-title">${board.title}</span>
                     <button class="board-add" data-number="${board.id}">Add Card</button>
+                    <button class="deleteBoard">Delete board <i class="fas fa-trash-alt"></i></button>
                     <button class="board-toggle" data-number="${board.id}"><i class="fas fa-chevron-down"></i></button>
                 </div>
             </section>`;
